031a232e00e7223530816ee3244dbde75f742017
jonas navarro <jonasr.navarro@gmail.com>
2025-03-08 14:13:07 +0800

    sec 9

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,8 +16,8 @@ public class AccountController(DataContext context, ITokenService tokenService) 
    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
    {
     var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
-    
-    if (user == null) return Unauthorized("Invalid username");
+
+    if (user == null) return Unauthorized("Invalid username1");
 
     using var hmac = new HMACSHA512(user.PasswordSalt);
     var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +8,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  http = inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
-  users: any;
+
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -27,11 +20,4 @@ export class HomeComponent implements OnInit {
     this.registerMode = event;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5125/api/users/').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Completed')
-    });
-  }
 }
